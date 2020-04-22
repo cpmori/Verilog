903bba69a550b590c845f42e16a8499c181254eb
Christopher Mori <cpmori@grendel25.ece.ncsu.edu>
2020-04-21 23:03:23 -0400

    Most recent working version

--- Project_2/adler32.v
+++ Project_2/adler32.v
@@ -4,92 +4,115 @@ module adler32(
 	data_valid,
 	last_data,
 	input [7:0] data,
-	output checksum_valid
-	output [31:0] checksum_valid
-);
-	
-	datapath D1();
-	controller C1();
+	output checksum_valid,
+	output [31:0] checksum
+);	
+	wire brief_reset;
+	datapath D1(
+		.data(data),
+		.clk(clock),
+		.rst_n(rst_n & brief_reset),
+		.checksum(checksum),
+		.data_valid(data_valid)
+	);
+	controller C1(
+		.clk(clock), 
+		.rst_n(rst_n),
+		.last_data(last_data),
+		.checksum_valid(checksum_valid),
+		.brief_reset(brief_reset)
+	);
 	
 endmodule
 
 
 module datapath(
 	input[7:0] data,
-	input clk,rst_n, 
+	input clk,rst_n, data_valid,
 	output[31:0] checksum
 );
 
-	wire[15:0] current_A,current_B, next_A, next_B, sum_A, sum_B, mod_A, mod_B;
+	wire[15:0] old_A,old_B, next_A, next_B, sum_A, sum_B, new_A, new_B;
 	
-	assign sum_A = data + current_A;
-	assign sum_B = current_B + next_A;
+	assign sum_A = data + old_A;
+	assign sum_B = old_B + new_A;
 	
 	modulus mod1(
 		.dividend(sum_A),
 		.divisor(65521),
-		.result(mod_A)
+		.result(new_A)
 	);
 	
-	modulus mod1(
+	modulus mod2(
 		.dividend(sum_B),
 		.divisor(65521),
-		.result(mod_B)
+		.result(new_B)
 	);
 	
-	dff_rstval #(
-		.WIDTH(16),
-		.RSTVAL(1))
-	A(.rst_n(rst_n), .clk(clock),.D(next_A),.Q(current_A));
+	dff_rstval #(.WIDTH(16),.RSTVAL(1))A(
+		.rst_n(rst_n), 
+		.clk(clk),
+		.D(next_A),
+		.Q(old_A)
+	);
 	
-	dff_rstval #(
-		.WIDTH(16),
-		.RSTVAL(0))
-	B(.rst_n(rst_n), . clk(clock), .D(next_B), .Q(current_B));
+	dff_rstval #(.WIDTH(16),.RSTVAL(0))B(
+		.rst_n(rst_n), 
+		.clk(clk), 
+		.D(next_B), 
+		.Q(old_B)
+	);
 	
-	assign next_A = data_valid ? mod_A : current_A;
-	assign next_B = data_valid ? mod_B : current_B;
 	
-	assign checksum[15:0] = current_A;
-	assign checksum[31:16] = current_B;
+		
+	assign next_A = data_valid ? new_A: old_A;
+	assign next_B = data_valid ? new_B: old_B;
+	
+	assign checksum[15:0] = old_A;
+	assign checksum[31:16] = old_B;
 	
 
 endmodule
 
-
 module controller(
 	input clk, rst_n, last_data,
-	output checksum_valid
+	output checksum_valid,brief_reset
 );	
 
-	wire current_state,next_state;
+	wire[1:0] current_state,next_state;
 	
-	dff #(.WIDTH(1)) DFF(
+	dff #(.WIDTH(2)) DFF(
 		.rst_n(rst_n),
 		.clk(clk),
 		.D(next_state),
 		.Q(current_state)
 	);
 	
-	assign next_state = last_data & ~(current_state);
-	assign checksum_valid = current_state;
+	assign next_state[0] = last_data & ~current_state[1] & ~current_state[0];
+	assign next_state[1] = current_state[0];
+	assign checksum_valid = ~current_state[1] & current_state[0];
+	assign brief_reset = ~current_state[1];
 
 endmodule
 	
 module modulus(
 	input[15:0] dividend,
-	input[31:0] divisor,
+	input[15:0] divisor,
 	output[15:0] result
 );
-	wire[31:0] sign_extend;
-	assign sign_extend[31:16] = 0;
-	assign sign_extend[15:0] = dividend; 
-	assign result = (sign_extend - divisor < 0) ? (sign_extend - divisor) : dividend;
+	wire[15:0] difference;
+
+	assign difference = dividend - divisor;
+
+	assign result = (divisor > dividend) ? dividend : difference; 
 
 endmodule
 	
 	
+
+module modulus_tb;
 	
+endmodule	
 	
 	
 	
